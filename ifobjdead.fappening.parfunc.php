7c5d3e3124c8d3e42a95e2283ed09ee3c803394d
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-16 04:47:18 +1000

    Latest update

--- ifobjdead.fappening.parfunc.php
+++ ifobjdead.fappening.parfunc.php
@@ -5,6 +5,6 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "tease") {
     $subSrc = $sub.'/footu.png';
-    $htmltag = htmlspecialchars("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
+    $htmltag = htmlentities("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
     echo $htmltag;
 }
--- onpartner.fappening.parfunc.php
+++ onpartner.fappening.parfunc.php
@@ -5,11 +5,11 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "tease") {
     $subSrc = $sub.'/footu.png';
-    $htmltag = htmlspecialchars("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
+    $htmltag = htmlentities("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
     echo $htmltag;
 } elseif ($subAction == "group") {
     $subSrc = $sub.'/footu.png';
     $objSrc = $obj.'/footu.png';
-    $htmltag = htmlspecialchars("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a> <a href='$objSrc'><img src='$objSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
+    $htmltag = htmlentities("<a href='$subSrc'><img src='$subSrc' style='width:20%;position:relative;'></a> <a href='$objSrc'><img src='$objSrc' style='width:20%;position:relative;'></a><br>", ENT_QUOTES);
     echo $htmltag;
 }
