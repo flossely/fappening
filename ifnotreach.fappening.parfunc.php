00c943525d2d7404b6415830bff882e9f68f2c1a
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-17 20:03:10 +1000

    Latest update

--- ifnotreach.fappening.parfunc.php
+++ ifnotreach.fappening.parfunc.php
@@ -4,8 +4,32 @@ $subActions = ["tease"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "tease") {
-    $subSrc = $sub.'/footu.png';
-    $htmlstr = "<a href='".$subSrc."'><img src='".$subSrc."' style='width:20%;position:relative;'></a><br>";
-    $htmltag = htmlentities($htmlstr);
-    echo $htmltag;
+    if (file_exists($sub.'/arousal')) {
+        $subArousal = file_get_contents($sub.'/arousal');
+    } else {
+        $subArousal = 1;
+    }
+    if (file_exists($obj.'/arousal')) {
+        $objArousal = file_get_contents($obj.'/arousal');
+    } else {
+        $objArousal = 1;
+    }
+    $subRating += 2.5 * $subArousal;
+    $objRating += 2.5 * $objArousal;
+    $subScore += 69 * $subArousal;
+    $objScore += 69 * $objArousal;
+    echo turnFormat($paradigm, $today) .
+        " : " .
+        $subModeSign .
+        $sub .
+        "[" .
+        $subRating .
+        "] " .
+        $spacedictus[$lingua]["sex"] .
+        " " .
+        $objModeSign .
+        $obj .
+        "[" .
+        $objRating .
+        "] <br>";
 }
--- onbeneficiary.fappening.parfunc.php
+++ onbeneficiary.fappening.parfunc.php
@@ -4,14 +4,61 @@ $subActions = ["tease","group"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "tease") {
-    $subSrc = $sub.'/footu.png';
-    $htmlstr = "<a href='".$subSrc."'><img src='".$subSrc."' style='width:20%;position:relative;'></a><br>";
-    $htmltag = htmlentities($htmlstr);
-    echo $htmltag;
+    if (file_exists($sub.'/arousal')) {
+        $subArousal = file_get_contents($sub.'/arousal');
+    } else {
+        $subArousal = 1;
+    }
+    if (file_exists($obj.'/arousal')) {
+        $objArousal = file_get_contents($obj.'/arousal');
+    } else {
+        $objArousal = 1;
+    }
+    $subRating += 2.5 * $subArousal;
+    $objRating += 2.5 * $objArousal;
+    $subScore += 69 * $subArousal;
+    $objScore += 69 * $objArousal;
+    echo turnFormat($paradigm, $today) .
+        " : " .
+        $subModeSign .
+        $sub .
+        "[" .
+        $subRating .
+        "] " .
+        $spacedictus[$lingua]["sex"] .
+        " " .
+        $objModeSign .
+        $obj .
+        "[" .
+        $objRating .
+        "] <br>";
 } elseif ($subAction == "group") {
-    $subSrc = $sub.'/footu.png';
-    $objSrc = $obj.'/footu.png';
-    $htmlstr = "<a href='".$subSrc."'><img src='".$subSrc."' style='width:20%;position:relative;'></a> <a href='".$objSrc."'><img src='".$objSrc."' style='width:20%;position:relative;'></a><br>";
-    $htmltag = htmlentities($htmlstr);
-    echo $htmltag;
+    if (file_exists($sub.'/arousal')) {
+        $subArousal = file_get_contents($sub.'/arousal');
+    } else {
+        $subArousal = 1;
+    }
+    if (file_exists($obj.'/arousal')) {
+        $objArousal = file_get_contents($obj.'/arousal');
+    } else {
+        $objArousal = 1;
+    }
+    $subRating += 2.5 * $subArousal;
+    $objRating += 2.5 * $objArousal;
+    $subScore += 69 * $subArousal;
+    $objScore += 69 * $objArousal;
+    echo turnFormat($paradigm, $today) .
+        " : " .
+        $subModeSign .
+        $sub .
+        "[" .
+        $subRating .
+        "] " .
+        $spacedictus[$lingua]["sex"] .
+        " " .
+        $objModeSign .
+        $obj .
+        "[" .
+        $objRating .
+        "] <br>";
 }
