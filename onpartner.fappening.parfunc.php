46b0e5d34a5e046a4373dca4ad4fdf0bf892203c
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 05:48:03 +1000

    Added new files

--- onpartner.fappening.parfunc.php
+++ onpartner.fappening.parfunc.php
@@ -4,8 +4,8 @@ $subActions = ['sex'];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "sex") {
-    $subRating += $subRating * $objArousal;
-    $objRating += $subRating * $subArousal;
+    $subRating += $objArousal * 10;
+    $objRating += $subArousal * 10;
     $subScore += $subArousal;
     $objScore += $objArousal;
     echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['sex'].' '.$objFullName.' ('.$objRating.')<br>';
