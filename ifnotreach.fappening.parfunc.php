cb68f4e8eb11199764e2a17df895bf218bf064bf
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-24 04:16:52 +1000

    Added new files

--- ifnotreach.fappening.parfunc.php
+++ ifnotreach.fappening.parfunc.php
@@ -1,23 +1,10 @@
 <?php
 
-$subActions = ["tease"];
+$subActions = ["fap"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
-if ($subAction == "tease") {
-    if (file_exists($sub.'/arousal')) {
-        $subArousal = file_get_contents($sub.'/arousal');
-    } else {
-        $subArousal = 1;
-    }
-    $subRating += 10 * $subArousal;
-    $subScore += 20 * $subArousal;
-    echo $turnNum .
-        " : " .
-        $subModeSign .
-        $sub .
-        "[" .
-        $subRating .
-        "] " .
-        $spacedictus[$proLingo]["fap"] .
-        "<br>";
+if ($subAction == "fap") {
+    $subRating += 2 * $subArousal;
+    $subScore += 2 * $subArousal;
+    echo $turnNum." : ".$subHalfNotation." ".$spacedictus[$proLingo]["fap"]."<br>";
 }
--- onbeneficiary.fappening.parfunc.php
+++ onbeneficiary.fappening.parfunc.php
@@ -1,52 +1,16 @@
 <?php
 
-$subActions = ["tease","group"];
+$subActions = ['fap', 'group'];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
-if ($subAction == "tease") {
-    if (file_exists($sub.'/arousal')) {
-        $subArousal = file_get_contents($sub.'/arousal');
-    } else {
-        $subArousal = 1;
-    }
-    $subRating += 10 * $subArousal;
-    $subScore += 20 * $subArousal;
-    echo $turnNum .
-        " : " .
-        $subModeSign .
-        $sub .
-        "[" .
-        $subRating .
-        "] " .
-        $spacedictus[$proLingo]["fap"] .
-        "<br>";
+if ($subAction == "fap") {
+    $subRating += 2 * $subArousal;
+    $subScore += 2 * $subArousal;
+    echo $turnNum." : ".$subHalfNotation." ".$spacedictus[$proLingo]["fap"]."<br>";
 } elseif ($subAction == "group") {
-    if (file_exists($sub.'/arousal')) {
-        $subArousal = file_get_contents($sub.'/arousal');
-    } else {
-        $subArousal = 1;
-    }
-    if (file_exists($obj.'/arousal')) {
-        $objArousal = file_get_contents($obj.'/arousal');
-    } else {
-        $objArousal = 1;
-    }
-    $subRating += 10 * $subArousal;
-    $objRating += 10 * $objArousal;
+    $subRating += 10 * $objArousal;
+    $objRating += 10 * $subArousal;
     $subScore += 20 * $subArousal;
     $objScore += 20 * $objArousal;
-    echo $turnNum .
-        " : " .
-        $subModeSign .
-        $sub .
-        "[" .
-        $subRating .
-        "] " .
-        $spacedictus[$proLingo]["sex"] .
-        " " .
-        $objModeSign .
-        $obj .
-        "[" .
-        $objRating .
-        "] <br>";
+    echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["sex"]." ".$objHalfNotation."<br>";
 }
