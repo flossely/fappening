9f9f9e7cd200929897b49503d042b4e3ce08f1f0
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-23 20:26:33 +1000

    Added new files

--- store.fappening.parfunc.php
+++ store.fappening.parfunc.php
@@ -5,17 +5,23 @@ $subUseWeapon = shopFor($sub, 'weapon');
 $objUseWeapon = shopFor($obj, 'weapon');
 
 if ($proUseWeapon !== null) {
+    $proForceType = $proUseWeapon['name'];
     $proForce = $proUseWeapon['damage'];
 } else {
+    $proForceType = 'melee';
     $proForce = 1;
 }
 if ($subUseWeapon !== null) {
+    $subForceType = $subUseWeapon['name'];
     $subForce = $subUseWeapon['damage'];
 } else {
+    $subForceType = 'melee';
     $subForce = 1;
 }
 if ($objUseWeapon !== null) {
+    $objForceType = $objUseWeapon['name'];
     $objForce = $objUseWeapon['damage'];
 } else {
+    $objForceType = 'melee';
     $objForce = 1;
 }
