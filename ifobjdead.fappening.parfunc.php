aaae2d27196037c821ca8911d94a44c15e51db4e
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-18 20:09:13 +0000

    Latest update

--- ifobjdead.fappening.parfunc.php
+++ ifobjdead.fappening.parfunc.php
@@ -4,7 +4,7 @@ $subActions = ["fap"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "fap") {
-    $subRating += $subArousal;
-    $subScore += $subArousal;
+    $subRating += 1;
+    $subScore += 1;
     echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['fap'].'<br>';
 }
--- onrr.fappening.parfunc.php
+++ onrr.fappening.parfunc.php
@@ -1,12 +1,14 @@
 <?php
 
-$subActions = ['sex'];
+$subActions = ['sex', 'foot'];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "sex") {
     $subRating += $objArousal * 10;
-    $objRating += $subArousal * 10;
-    $subScore += $subArousal;
     $objScore += $objArousal;
     echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['sex'].' '.$objFullName.' ('.$objRating.')<br>';
+} elseif ($subAction == "foot") {
+    $subRating += $objArousal * 100;
+    $objScore += $objArousal;
+    echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['foot'].' '.$objFullName.' ('.$objRating.')<br>';
 }
