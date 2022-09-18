eebb204e61b1b7311fdc5e9764a5c0ff93aca9e3
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-18 20:15:33 +0000

    Latest update

--- onrr.fappening.parfunc.php
+++ onrr.fappening.parfunc.php
@@ -1,14 +1,10 @@
 <?php
 
-$subActions = ['sex', 'foot'];
+$subActions = ['sex'];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "sex") {
-    $subRating += $objArousal * 10;
+    $subRating *= $objArousal;
     $objScore += $objArousal;
     echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['sex'].' '.$objFullName.' ('.$objRating.')<br>';
-} elseif ($subAction == "foot") {
-    $subRating += $objArousal * 100;
-    $objScore += $objArousal;
-    echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['foot'].' '.$objFullName.' ('.$objRating.')<br>';
 }
