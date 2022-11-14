bfb5a5d32c5698d012e8ecc6644201a95081b4ce
Karl August Nestor <jesuscaesar@outlook.com>
2022-11-14 11:13:16 +0000

    Latest update

--- ifself.fappening.parfunc.php
+++ ifself.fappening.parfunc.php
@@ -1,10 +1,9 @@
 <?php
 
-$subActions = ["fap"];
+$subActions = ["pass"];
 $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
-if ($subAction == "fap") {
-    $subRating += 1;
-    $subScore += 1;
-    echo $turnNum.' : '.$subFullName.' ('.$subRating.') '.$spacedictus[$proLingo]['fap'].'<br>';
+
+if ($subAction == "pass") {
+    echo $turnNum." : ".$subFullName.' '.$spacedictus[$proLingo]["pass"]."<br>";
 }
