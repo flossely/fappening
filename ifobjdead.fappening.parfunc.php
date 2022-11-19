4925674af28438c9d82115636e0c9cf5f9b1f0d8
Karl August Nestor <jesuscaesar@outlook.com>
2022-11-19 06:40:07 +0000

    Latest update

--- ifobjdead.fappening.parfunc.php
+++ ifobjdead.fappening.parfunc.php
@@ -5,5 +5,5 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "pass") {
-    echo $turnNum." : ".$subFullName.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    echo $turnNum." : ".$subFullName.' '.$actbook[$proLingo]['200']."<br>";
 }
