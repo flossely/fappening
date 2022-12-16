b4110eada92e5e2841a40d8cdeb21b861f5ca7fd
Karl August Nestor <jesuscaesar@outlook.com>
2022-12-16 11:01:56 +0000

    Latest update

--- post.fappening.parfunc.php
+++ post.fappening.parfunc.php
@@ -1,6 +1,3 @@
 <?php
 
-file_put_contents($obj.'/ratio', $objRatio);
-chmod($obj.'/ratio', 0777);
-file_put_contents($sub.'/ratio', $subRatio);
-chmod($sub.'/ratio', 0777);
+
--- pre.fappening.parfunc.php
+++ pre.fappening.parfunc.php
@@ -10,7 +10,27 @@ if (file_get_contents($sub.'/ratio')) {
 } else {
     $subRatio = 0;
 }
-$subArousal = 10 ** $subRatio;
+
+if (file_get_contents($sub.'/bshp')) {
+    $subBshpFile = file_get_contents($sub.'/bshp');
+    if (is_numeric($subBshpFile)) {
+        $subBshp = $subBshpFile;
+    } else {
+        $subBshp = 1;
+    }
+} else {
+    $subBshp = 1;
+}
+if (file_get_contents($sub.'/fshp')) {
+    $subFshpFile = file_get_contents($sub.'/fshp');
+    if (is_numeric($subFshpFile)) {
+        $subFshp = $subFshpFile;
+    } else {
+        $subFshp = 1;
+    }
+} else {
+    $subFshp = 1;
+}
 
 if (file_get_contents($obj.'/ratio')) {
     $objRatioFile = file_get_contents($obj.'/ratio');
@@ -22,4 +42,30 @@ if (file_get_contents($obj.'/ratio')) {
 } else {
     $objRatio = 0;
 }
-$objArousal = 10 ** $objRatio;
+
+if (file_get_contents($obj.'/bshp')) {
+    $objBshpFile = file_get_contents($obj.'/bshp');
+    if (is_numeric($objBshpFile)) {
+        $objBshp = $objBshpFile;
+    } else {
+        $objBshp = 1;
+    }
+} else {
+    $objBshp = 1;
+}
+if (file_get_contents($obj.'/fshp')) {
+    $objFshpFile = file_get_contents($obj.'/fshp');
+    if (is_numeric($objFshpFile)) {
+        $objFshp = $objFshpFile;
+    } else {
+        $objFshp = 1;
+    }
+} else {
+    $objFshp = 1;
+}
+
+$subLibido = 10 ** $subFshp;
+$objLibido = 10 ** $objFshp;
+
+$subArousal = round($subLibido * ($subRatio / 4));
+$objArousal = round($objLibido * ($objRatio / 4));
